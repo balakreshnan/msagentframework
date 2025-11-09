a1e792de5825b3f7db1d0813fe1938a11be9c12d
balakreshnan <babal@microsoft.com>
2025-11-09 09:38:44 -0600

    updated

---
+++ stagteval.py
@@ -0,0 +1,109 @@
+import asyncio
+import os
+from random import randint
+from typing import Annotated
+from urllib import response
+
+from agent_framework import ChatAgent
+from agent_framework.azure import AzureAIAgentClient
+from azure.ai.projects.aio import AIProjectClient
+from azure.identity.aio import AzureCliCredential
+from pydantic import Field
+import os
+from azure.ai.evaluation import ToolCallAccuracyEvaluator, AzureOpenAIModelConfiguration
+from azure.ai.evaluation import IntentResolutionEvaluator, TaskAdherenceEvaluator, ResponseCompletenessEvaluator
+
+from pprint import pprint
+
+from dotenv import load_dotenv
+
+# Load environment variables
+load_dotenv()
+
+def get_weather(
+    location: Annotated[str, Field(description="The location to get the weather for.")],
+) -> str:
+    """Get the weather for a given location."""
+    conditions = ["sunny", "cloudy", "rainy", "stormy"]
+    return f"The weather in {location} is {conditions[randint(0, 3)]} with a high of {randint(10, 30)}°C."
+
+model_config = AzureOpenAIModelConfiguration(
+    azure_endpoint=os.environ["AZURE_OPENAI_ENDPOINT"],
+    api_key=os.environ["AZURE_OPENAI_KEY"],
+    api_version=os.environ["AZURE_OPENAI_API_VERSION"],
+    azure_deployment=os.environ["AZURE_OPENAI_DEPLOYMENT"],
+)
+
+
+tool_call_accuracy = ToolCallAccuracyEvaluator(model_config=model_config)
+
+async def main() -> None:
+    print("=== Azure AI Chat Client with Existing Agent ===")
+
+    # Create the client
+    async with (
+        AzureCliCredential() as credential,
+        AIProjectClient(endpoint=os.environ["AZURE_AI_PROJECT_ENDPOINT"], credential=credential) as client,
+    ):
+        # Create an agent that will persist
+        created_agent = await client.agents.create_agent(
+            model=os.environ["AZURE_AI_MODEL_DEPLOYMENT_NAME"], name="WeatherAgent"
+        )
+        query = "What's the weather like in Tokyo?"
+        try:
+            async with ChatAgent(
+                # passing in the client is optional here, so if you take the agent_id from the portal
+                # you can use it directly without the two lines above.
+                chat_client=AzureAIAgentClient(project_client=client, agent_id=created_agent.id),
+                instructions="You are a helpful weather agent.",
+                tools=get_weather,
+            ) as agent:
+                result = await agent.run(query)
+                print(f"Result: {result}\n")
+
+                # query = "How is the weather in Seattle ?"
+                tool_call = {
+                    "type": "tool_call",
+                    "tool_call_id": "call_CUdbkBfvVBla2YP3p24uhElJ",
+                    "name": "fetch_weather",
+                    "arguments": {"location": "Seattle"},
+                }
+
+                tool_definition = {
+                    "id": "fetch_weather",
+                    "name": "fetch_weather",
+                    "description": "Fetches the weather information for the specified location.",
+                    "parameters": {
+                        "type": "object",
+                        "properties": {"location": {"type": "string", "description": "The location to fetch weather for."}},
+                    },
+                }
+                response = tool_call_accuracy(query=query, tool_calls=tool_call, tool_definitions=tool_definition)
+                pprint(response)
+                response_completeness_evaluator = ResponseCompletenessEvaluator(model_config=model_config)
+                result = response_completeness_evaluator(
+                    response=result,
+                    ground_truth=result,
+                )
+                pprint(result)
+                intent_resolution_evaluator = IntentResolutionEvaluator(model_config)
+                # Success example. Intent is identified and understood and the response correctly resolves user intent
+                result = intent_resolution_evaluator(
+                    query=query,
+                    response=result,
+                )
+                pprint(result)
+                task_adherence_evaluator = TaskAdherenceEvaluator(model_config)
+                result = task_adherence_evaluator(
+                    query=query,
+                    response=result,
+                )
+                pprint(result)
+        finally:
+            # Clean up the agent manually
+            
+            await client.agents.delete_agent(created_agent.id)
+
+
+if __name__ == "__main__":
+    asyncio.run(main())
